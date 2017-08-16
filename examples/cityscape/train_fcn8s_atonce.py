b903f6f40417f687ac463c6b74c5a18c705d4988
Bicheng Gao <volz.kz.g@gmail.com>
2017-08-16 14:47:10 +0000

    update training schema for train_fcn8s_atonce

--- examples/cityscape/train_fcn8s_atonce.py
+++ examples/cityscape/train_fcn8s_atonce.py
@@ -22,6 +22,13 @@ configurations = {
         weight_decay=0.0005,
         interval_validate=4000,
         fcn16s_pretrained_model=torchfcn.models.FCN16s.pretrained_model,
+    ),
+    2: dict(
+        max_iteration=100000,
+        lr=1.0e-4,
+        momentum=0.99,
+        weight_decay=0.0005,
+        interval_validate=4000,
     )
 }
 
@@ -49,6 +56,11 @@ def main():
     if cuda:
         torch.cuda.manual_seed(1337)
 
+    if torch.cuda.device_count() == 1:
+        batch_size = 1
+    else:
+        batch_size = 2 * torch.cuda.device_count()
+
     # 1. dataset
 
     root = osp.expanduser('~/data/datasets')
@@ -56,17 +68,17 @@ def main():
     mix_loader = torch.utils.data.DataLoader(
         torchfcn.datasets.CityScapesClassSeg(
             root, split=['train', 'val'], transform=True, preprocess=False,
-        ), batch_size=1, shuffle=True, **kwargs
+        ), batch_size=batch_size, shuffle=True, **kwargs
     )
     train_loader = torch.utils.data.DataLoader(
         torchfcn.datasets.CityScapesClassSeg(
             root, split=['train'], transform=True, preprocess=False,
-        ), batch_size=1, shuffle=True, **kwargs
+        ), batch_size=batch_size, shuffle=True, **kwargs
     )
     val_loader = torch.utils.data.DataLoader(
         torchfcn.datasets.CityScapesClassSeg(
             root, split=['val'], transform=True, preprocess=False,
-        ), batch_size=1, shuffle=False, **kwargs
+        ), batch_size=batch_size, shuffle=False, **kwargs
     )
 
     # train_loader = torch.utils.data.DataLoader(
@@ -92,18 +104,21 @@ def main():
         vgg16 = torchfcn.models.VGG16(pretrained=True)
         model.copy_params_from_vgg16(vgg16)
     if cuda:
-        model = model.cuda()
+        if torch.cuda.device_count() == 1:
+            model = model.cuda()
+        else:
+            model = torch.nn.DataParallel(model).cuda()
 
     # 3. optimizer
 
-    optim = torch.optim.SGD(
+    optim = torch.optim.Adam(
         [
             {'params': get_parameters(model, bias=False)},
             {'params': get_parameters(model, bias=True),
              'lr': cfg['lr'] * 2, 'weight_decay': 0},
         ],
         lr=cfg['lr'],
-        momentum=cfg['momentum'],
+        # momentum=cfg['momentum'],
         weight_decay=cfg['weight_decay'])
     if resume:
         optim.load_state_dict(checkpoint['optim_state_dict'])
