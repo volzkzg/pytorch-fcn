14d986353d7b5762e5af418fd81e5db7d89e5551
Bicheng Gao <volz.kz.g@gmail.com>
2017-08-17 21:08:42 +0000

    update make_imagesets

--- examples/cityscape/make_imagesets.py
+++ examples/cityscape/make_imagesets.py
@@ -2,7 +2,7 @@ import os
 import os.path as osp
 
 root = osp.expanduser('~/data/datasets')
-for split in ['train', 'val']:
+for split in ['train', 'val', 'test']:
     dataset_dir = osp.join(root, 'cityscapes/')
 
     img_dir = osp.join(dataset_dir, 'leftImg8bit/%s' % split)
